f3a8b880a8581b47762e428c5f2d935efdf70cc6
Cyril <Cyril@CYLAPTOP>
2025-01-29 18:03:22 +0800

    first commit

---
+++ Login/Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LoginModel;
+
+namespace Login.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(LoginModel.Login login)
+        {
+           if (ModelState.IsValid)
+           {
+               return RedirectToAction("Index", "Home");
+           }
+           return View(login);
+
+        //    if (login.Username == "admin" && login.Password == "admin")
+        //     {
+        //         return RedirectToAction("Index", "Home");
+        //     }
+        //     else
+        //     {
+        //         return View();
+        //     }
+        }
+    }
+}
